d02a918486a174d9eb35faedb258c24799fc6ff4
rajat3154 <rajaturanvir31@gmail.com>
2025-09-25 19:58:07 +0530

    Updated readme and queries.sql

--- queries.sql
+++ queries.sql
@@ -6,21 +6,12 @@ SELECT * FROM students ORDER BY age DESC LIMIT 3;
 
 SELECT DISTINCT grade FROM students;
 
-SELECT s.name, s.grade, c.course_name
-FROM students s
-INNER JOIN courses c ON s.id = c.student_id;
+SELECT * FROM students WHERE grade='A' OR age=18;
 
-SELECT s.name, c.course_name
-FROM students s
-LEFT JOIN courses c ON s.id = c.student_id;
+SELECT * FROM students WHERE name LIKE "A%";
 
-SELECT s.name, s.grade, c.course_name
-FROM students s
-INNER JOIN courses c ON s.id = c.student_id
-WHERE s.grade = 'A' AND c.course_name = 'Machine Learning';
+SELECT * FROM students WHERE age BETWEEN 19 AND 22;
 
-SELECT s.name, COUNT(c.course_id) AS total_courses
-FROM students s
-LEFT JOIN courses c ON s.id = c.student_id
-GROUP BY s.name
-ORDER BY total_courses DESC;
+SELECT * FROM students WHERE grade IN ('A','B');
+
+SELECT grade, COUNT(*) AS total_students FROM students GROUP BY grade;
